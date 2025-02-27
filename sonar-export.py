d0e1d6c1b52761d446a68bd177a1bf794991e6a3
Eric Pastoor <epastoor@jaggaer.com>
2025-02-27 13:12:32 -0500

    POT-3992 Added changes to accept inputs for repo and types of issues

--- sonar-export.py
+++ sonar-export.py
@@ -1,12 +1,24 @@
+import argparse
 import pandas as pd
 import requests
 import base64
 from datetime import datetime, timedelta
 
+# Command-line arguments
+parser = argparse.ArgumentParser(description='Fetch issues from SonarQube.')
+parser.add_argument('--project', required=True, help='Project Key')
+parser.add_argument('--token', required=True, help='Project Token')
+parser.add_argument('--types', required=True, help='Comma-separated list of issue types: CODE_SMELL, BUG, VULNERABILITY')
+args = parser.parse_args()
+
+PROJECT_KEY = args.project
+TOKEN = args.token
+TYPES = args.types # Accepts a comma-separated list of issue types
+
 # SonarQube parameters
-SONARQUBE_URL = 'http://localhost:9000/api/issues/search' #Sonar Instance URL
-PROJECT_KEY = '' #Your Project Key
-TOKEN = '' #Your Project Token
+SONARQUBE_URL = 'https://jag-dev-sonarqube.va.jaggaer.com/api/issues/search' #Sonar Instance URL
+TODAY=pd.to_datetime('today').strftime('%Y-%m-%d')
+OUTPUT_FILE = f'sonarqube_issues_{PROJECT_KEY.replace(":", "_")}_{TYPES.replace(",", "-")}_{TODAY}.xlsx'
 
 # Fetch issues from SonarQube
 auth = base64.b64encode(f'{TOKEN}:'.encode()).decode()
@@ -28,12 +40,15 @@ while current_start_date < end_date:
 
     params = { #Ajdust as required
         'componentKeys': PROJECT_KEY,
+	'types': TYPES,
         'createdAfter': current_start_date.strftime('%Y-%m-%d'),
         'createdBefore': current_end_date.strftime('%Y-%m-%d'),
         'ps': page_size,
         'p': 1
     }
 
+    print(f'Fetching issues from {current_start_date.strftime("%Y-%m-%d")} to {current_end_date.strftime("%Y-%m-%d")}...')
+    print(f'Request parameters: {params}')
     while True:
         response = requests.get(SONARQUBE_URL, headers=headers, params=params)
         
@@ -63,7 +78,7 @@ if all_issues:
     # Convert to DataFrame
     df = pd.DataFrame(all_issues)
     # Save to Excel
-    df.to_excel('sonarqube_issues.xlsx', index=False)
-    print('Issues exported to sonarqube_issues.xlsx')
+    df.to_excel(OUTPUT_FILE, index=False)
+    print('Issues exported to ', OUTPUT_FILE)
 else:
     print('No issues found.')
